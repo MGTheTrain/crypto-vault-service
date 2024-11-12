5a7a3ba3cc21504af617de1fbe668d46966a0c21
Marvin Gajek <Marvin.Gajek1@web.de>
2024-11-12 15:16:09 +0100

    support pkcs#1 and pkcs#8 formats

--- pkg/cryptography/encryption.go
+++ pkg/cryptography/encryption.go
@@ -181,9 +181,22 @@ func ReadPrivateKey(privateKeyPath string) (*rsa.PrivateKey, error) {
 		return nil, fmt.Errorf("failed to parse PEM block containing the private key")
 	}
 
+	// First try to parse as PKCS#1 format
 	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err == nil {
+		return privateKey, nil
+	}
+
+	// If PKCS#1 parsing fails, try parsing as PKCS#8 format
+	privateKeyInterface, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse private key: %v", err)
+		return nil, fmt.Errorf("unable to parse private key in either PKCS#1 or PKCS#8 format: %v", err)
+	}
+
+	// Type assertion to *rsa.PrivateKey if it is indeed an RSA key
+	privateKey, ok := privateKeyInterface.(*rsa.PrivateKey)
+	if !ok {
+		return nil, fmt.Errorf("private key is not of type RSA")
 	}
 
 	return privateKey, nil
@@ -201,9 +214,22 @@ func ReadPublicKey(publicKeyPath string) (*rsa.PublicKey, error) {
 		return nil, fmt.Errorf("failed to parse PEM block containing the public key")
 	}
 
+	// Try to parse as PKCS#1 format first
 	publicKey, err := x509.ParsePKCS1PublicKey(block.Bytes)
+	if err == nil {
+		return publicKey, nil
+	}
+
+	// If PKCS#1 parsing fails, try parsing as PKCS#8 format
+	pubKeyInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
 	if err != nil {
-		return nil, fmt.Errorf("unable to parse public key: %v", err)
+		return nil, fmt.Errorf("unable to parse public key in either PKCS#1 or PKCS#8 format: %v", err)
+	}
+
+	// Type assertion to *rsa.PublicKey if it is indeed an RSA key
+	publicKey, ok := pubKeyInterface.(*rsa.PublicKey)
+	if !ok {
+		return nil, fmt.Errorf("public key is not of type RSA")
 	}
 
 	return publicKey, nil
