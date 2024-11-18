ba449519c0d3a952fe64ea9bfe31eca37ca169dc
Marvin Gajek <Marvin.Gajek1@web.de>
2024-11-18 10:46:21 +0100

    reduce entities

--- internal/domain/model/blob.go
+++ internal/domain/model/blob.go
@@ -4,13 +4,16 @@ import "time"
 
 // Blob represents metadata on the actual blob being stored
 type Blob struct {
-	BlobID           string `gorm:"primaryKey"`
-	BlobStoragePath  string
-	UploadTime       time.Time
-	UserID           string
-	BlobName         string
-	BlobSize         int
-	BlobType         string
-	Metadata         []Metadata
-	CryptographicKey CryptographicKey `gorm:"foreignKey:KeyID"`
+	BlobID              string `gorm:"primaryKey"`
+	BlobStoragePath     string
+	UploadTime          time.Time
+	UserID              string
+	BlobName            string
+	BlobSize            int
+	BlobType            string
+	EncryptionAlgorithm string
+	HashAlgorithm       string
+	IsEncrypted         bool
+	IsSigned            bool
+	CryptographicKey    CryptographicKey `gorm:"foreignKey:KeyID"`
 }
--- internal/domain/model/key.go
+++ internal/domain/model/key.go
@@ -5,11 +5,8 @@ import "time"
 // CryptographicKey represents the encryption key entity
 type CryptographicKey struct {
 	KeyID     string `gorm:"primaryKey"`
-	KeyValue  string
 	KeyType   string
 	CreatedAt time.Time
 	ExpiresAt time.Time
 	UserID    string `gorm:"index"`
-	Metadata  []Metadata
-	Blobs     []Blob
 }
