234236732d12de52a1607b1de1523284b1f57930
Marvin Gajek <Marvin.Gajek1@web.de>
2024-11-18 10:34:23 +0100

    replace file with blob

--- internal/domain/model/blob.go
+++ internal/domain/model/blob.go
@@ -2,15 +2,15 @@ package model
 
 import "time"
 
-// Blob represents metadata on the actual file being stored
+// Blob represents metadata on the actual blob being stored
 type Blob struct {
 	BlobID           string `gorm:"primaryKey"`
 	BlobStoragePath  string
 	UploadTime       time.Time
 	UserID           string
-	FileName         string
-	FileSize         int
-	FileType         string
+	BlobName         string
+	BlobSize         int
+	BlobType         string
 	Metadata         []Metadata
 	CryptographicKey CryptographicKey `gorm:"foreignKey:KeyID"`
 }
