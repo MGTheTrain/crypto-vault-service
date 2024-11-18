7a4d4f97f1834dc30e54d6c5a74c1bbbe0ae3b7d
Marvin Gajek <Marvin.Gajek1@web.de>
2024-11-18 14:00:37 +0100

    separate concerns

--- internal/domain/contracts/metadata_management.go
+++ internal/domain/contracts/metadata_management.go
@@ -4,27 +4,26 @@ import (
 	"crypto_vault_service/internal/domain/model"
 )
 
-// MetadataManagement defines the methods for managing Blob and CryptographicKey metadata
-type MetadataManagement interface {
-	// ---CRUD operations for Blob metadata---
+// BlobMetadataManagement defines the methods for managing Blob metadata
+type BlobMetadataManagement interface {
+	// Create creates a new blob
+	Create(blob *model.Blob) (*model.Blob, error)
+	// Get retrieves blob by ID
+	Get(blobID string) (*model.Blob, error)
+	// Update updates a blob's metadata
+	Update(blobID string, updates *model.Blob) (*model.Blob, error)
+	// Delete deletes a blob by ID
+	Delete(blobID string) error
+}
 
-	// CreateBlob creates a new blob
-	CreateBlob(blob *model.Blob) (*model.Blob, error)
-	// GetBlob retrieves blob by ID
-	GetBlob(blobID string) (*model.Blob, error)
-	// UpdateBlob updates a blob's metadata
-	UpdateBlob(blobID string, updates *model.Blob) (*model.Blob, error)
-	// DeleteBlob deletes a blob by ID
-	DeleteBlob(blobID string) error
-
-	// ---CRUD operations for CryptographicKey metadata---
-
-	// CreateCryptographicKey creates a new cryptographic key
-	CreateCryptographicKey(key *model.CryptographicKey) (*model.CryptographicKey, error)
-	// GetCryptographicKey retrieves cryptographic key by ID
-	GetCryptographicKey(keyID string) (*model.CryptographicKey, error)
-	// UpdateCryptographicKey updates cryptographic key metadata
-	UpdateCryptographicKey(keyID string, updates *model.CryptographicKey) (*model.CryptographicKey, error)
-	// DeleteCryptographicKey deletes a cryptographic key by ID
-	DeleteCryptographicKey(keyID string) error
+// CryptographicKeyMetadataManagement defines the methods for managing CryptographicKey metadata
+type CryptographicKeyMetadataManagement interface {
+	// Create creates a new cryptographic key
+	Create(key *model.CryptographicKey) (*model.CryptographicKey, error)
+	// Get retrieves cryptographic key by ID
+	Get(keyID string) (*model.CryptographicKey, error)
+	// Update updates cryptographic key metadata
+	Update(keyID string, updates *model.CryptographicKey) (*model.CryptographicKey, error)
+	// Delete deletes a cryptographic key by ID
+	Delete(keyID string) error
 }
