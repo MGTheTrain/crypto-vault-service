a9dbfae171c89a3cbe715071a17354cdb19c2e61
root <Marvin.Gajek1@web.de>
2024-11-20 12:34:36 +0000

    add unit tests for blob service implementations

---
+++ test/unit/app/services/mocks.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"bytes"
+	"crypto_vault_service/internal/domain/blobs"
+
+	"github.com/stretchr/testify/mock"
+)
+
+// MockBlobConnector is a mock for the BlobConnector interface
+type MockBlobConnector struct {
+	mock.Mock
+}
+
+func (m *MockBlobConnector) Upload(filePaths []string) ([]*blobs.BlobMeta, error) {
+	args := m.Called(filePaths)
+	return args.Get(0).([]*blobs.BlobMeta), args.Error(1)
+}
+
+func (m *MockBlobConnector) Delete(blobID, blobName string) error {
+	args := m.Called(blobID, blobName)
+	return args.Error(0)
+}
+
+func (m *MockBlobConnector) Download(blobID, blobName string) (*bytes.Buffer, error) {
+	args := m.Called(blobID, blobName)
+	return args.Get(0).(*bytes.Buffer), args.Error(1)
+}
+
+// MockBlobRepository is a mock for the BlobRepository interface
+type MockBlobRepository struct {
+	mock.Mock
+}
+
+// Create mocks the Create method of the BlobRepository interface
+func (m *MockBlobRepository) Create(blob *blobs.BlobMeta) error {
+	args := m.Called(blob)
+	return args.Error(0)
+}
+
+// GetById mocks the GetById method of the BlobRepository interface
+func (m *MockBlobRepository) GetById(blobID string) (*blobs.BlobMeta, error) {
+	args := m.Called(blobID)
+	return args.Get(0).(*blobs.BlobMeta), args.Error(1)
+}
+
+// UpdateById mocks the UpdateById method of the BlobRepository interface
+func (m *MockBlobRepository) UpdateById(blob *blobs.BlobMeta) error {
+	args := m.Called(blob)
+	return args.Error(0)
+}
+
+// DeleteById mocks the DeleteById method of the BlobRepository interface
+func (m *MockBlobRepository) DeleteById(blobID string) error {
+	args := m.Called(blobID)
+	return args.Error(0)
+}
