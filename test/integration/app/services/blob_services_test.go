3179fe770dacceb4639b1cef66258e1d76cacee3
Marvin Gajek <Marvin.Gajek1@web.de>
2024-12-19 13:30:28 +0100

    consider test case for successful blob upload with encryption and signing

--- test/integration/app/services/blob_services_test.go
+++ test/integration/app/services/blob_services_test.go
@@ -17,10 +17,11 @@ import (
 )
 
 type BlobServicesTest struct {
-	BlobUploadService   *services.BlobUploadService
-	BlobDownloadService *services.BlobDownloadService
-	BlobMetadataService *services.BlobMetadataService
-	DBContext           *helpers.TestDBContext
+	BlobUploadService      *services.BlobUploadService
+	BlobDownloadService    *services.BlobDownloadService
+	BlobMetadataService    *services.BlobMetadataService
+	CryptoKeyUploadService *services.CryptoKeyUploadService
+	DBContext              *helpers.TestDBContext
 }
 
 func NewBlobServicesTest(t *testing.T) *BlobServicesTest {
@@ -43,10 +44,11 @@ func NewBlobServicesTest(t *testing.T) *BlobServicesTest {
 	blobConnector, err := connector.NewAzureBlobConnector(blobConnectorSettings, logger)
 	require.NoError(t, err, "Error creating blob connector")
 
-	vaultConnector, err := connector.NewAzureVaultConnector(&settings.KeyConnectorSettings{
+	keyConnectorSettings := &settings.KeyConnectorSettings{
 		ConnectionString: "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;",
 		ContainerName:    "testblobs",
-	}, logger)
+	}
+	vaultConnector, err := connector.NewAzureVaultConnector(keyConnectorSettings, logger)
 	require.NoError(t, err, "Error creating vault connector")
 
 	blobUploadService := services.NewBlobUploadService(blobConnector, ctx.BlobRepo, vaultConnector, ctx.CryptoKeyRepo, logger)
@@ -58,16 +60,51 @@ func NewBlobServicesTest(t *testing.T) *BlobServicesTest {
 	blobMetadataService := services.NewBlobMetadataService(ctx.BlobRepo, blobConnector, logger)
 	require.NoError(t, err, "Error creating BlobMetadataService")
 
+	cryptoKeyUploadService, err := services.NewCryptoKeyUploadService(vaultConnector, ctx.CryptoKeyRepo, logger)
+	require.NoError(t, err, "Error creating CryptoKeyUploadService")
+
 	return &BlobServicesTest{
-		BlobUploadService:   blobUploadService,
-		BlobDownloadService: blobDownloadService,
-		BlobMetadataService: blobMetadataService,
-		DBContext:           ctx,
+		BlobUploadService:      blobUploadService,
+		BlobDownloadService:    blobDownloadService,
+		BlobMetadataService:    blobMetadataService,
+		CryptoKeyUploadService: cryptoKeyUploadService,
+		DBContext:              ctx,
 	}
 }
 
 // Test case for successful blob upload with encryption and signing
-func TestBlobUploadService_Upload_Success(t *testing.T) {
+func TestBlobUploadService_Upload_With_Encryption_And_Signing_Success(t *testing.T) {
+	blobServices := NewBlobServicesTest(t)
+	defer helpers.TeardownTestDB(t, blobServices.DBContext, "sqlite")
+
+	testFileContent := []byte("This is test file content")
+	testFileName := "testfile.txt"
+
+	form, err := helpers.CreateTestFileAndForm(t, testFileName, testFileContent)
+	require.NoError(t, err)
+
+	userId := uuid.New().String()
+
+	keyPairId := uuid.New().String()
+	keyAlgorithm := "RSA"
+	keySize := 2048
+
+	cryptoKeyMetas, err := blobServices.CryptoKeyUploadService.Upload(userId, keyPairId, keyAlgorithm, uint(keySize))
+	require.NoError(t, err)
+	require.Equal(t, len(cryptoKeyMetas), 2)
+
+	signKeyId := cryptoKeyMetas[0].ID       // private key
+	encryptionKeyId := cryptoKeyMetas[1].ID // public key
+
+	blobMetas, err := blobServices.BlobUploadService.Upload(form, userId, &encryptionKeyId, &signKeyId)
+	require.NoError(t, err)
+	require.NotNil(t, blobMetas)
+	require.NotEmpty(t, blobMetas[0].ID)
+	require.Equal(t, userId, blobMetas[0].UserID)
+}
+
+// Test case for successful blob upload without encryption and signing
+func TestBlobUploadService_Upload_Without_Encryption_And_Signing_Success(t *testing.T) {
 	blobServices := NewBlobServicesTest(t)
 	defer helpers.TeardownTestDB(t, blobServices.DBContext, "sqlite")
 
