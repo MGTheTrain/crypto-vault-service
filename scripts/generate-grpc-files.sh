a2ca6f225838b70b696243eb9e7b754e7c571f43
MGTheTrain <marvin.gajek@outlook.com>
2024-12-23 15:17:31 +0000

    modify protoc command in script, run script to generate gateway Go file

--- scripts/generate-grpc-files.sh
+++ scripts/generate-grpc-files.sh
@@ -14,9 +14,12 @@ NC='\033[0m'
 echo "#####################################################################################################"
 echo -e "$BLUE INFO: $NC About to generate Go gRPC files from proto files"
 
-ls
-protoc --go_out=../generated --go-grpc_out=../generated ./internal/service.proto
-# protoc -I . --go_out . --go_opt . --go-grpc_out . --go-grpc_opt . --grpc-gateway_out . --grpc-gateway_opt . ./service.proto
+protoc -I ./ \
+  --go_out=../generated \
+  --go-grpc_out=../generated \
+  --grpc-gateway_out=../generated \
+  --grpc-gateway_opt=logtostderr=true \
+  ./internal/service.proto
 
 find . -type f -name '*.go' -exec sed -i 's/^package __/package generated/' {} \;
 
