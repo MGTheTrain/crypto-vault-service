7aecdbc0e9b65c4f0b89bbcaf744e294858fe9ae
MGTheTrain <marvin.gajek@outlook.com>
2024-12-23 16:53:16 +0000

    install grpcurl in devcontainer

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,3 +4,4 @@ apt-get update
 apt-get install -y openssl opensc softhsm libssl-dev libengine-pkcs11-openssl protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
