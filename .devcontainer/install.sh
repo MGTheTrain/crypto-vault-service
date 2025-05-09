77cb2306a2023b17b12d55a9c0b342ae6613ce14
MGTheTrain <marvin.gajek1@web.de>
2025-05-09 14:53:04 +0000

    add tooling for auto-formatting markdown files and shell scripts

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -6,3 +6,9 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 go install golang.org/x/tools/cmd/goimports@latest
+
+# shfmt for bash script formatting
+apt-get install -y shfmt
+
+# prettier for Markdown formatting
+npm install -g prettier
