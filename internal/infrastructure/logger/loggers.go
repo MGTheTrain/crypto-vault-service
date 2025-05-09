78e5a072d67e619267156382e14bf39fe6e5974b
MGTheTrain <marvin.gajek1@web.de>
2025-05-09 11:25:01 +0000

    make structs private

--- internal/infrastructure/logger/loggers.go
+++ internal/infrastructure/logger/loggers.go
@@ -18,48 +18,48 @@ type Logger interface {
 	Panic(args ...interface{})
 }
 
-// ConsoleLogger is an implementation of Logger that logs to the console.
-type ConsoleLogger struct {
+// consoleLogger is an implementation of Logger that logs to the console.
+type consoleLogger struct {
 	logger *logrus.Logger
 }
 
 // NewConsoleLogger creates a new console logger with the specified log level.
-func NewConsoleLogger(level logrus.Level) *ConsoleLogger {
+func NewConsoleLogger(level logrus.Level) *consoleLogger {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp: true,
 	})
 	logger.SetLevel(level)
-	return &ConsoleLogger{logger: logger}
+	return &consoleLogger{logger: logger}
 }
 
-func (l *ConsoleLogger) Info(args ...interface{}) {
+func (l *consoleLogger) Info(args ...interface{}) {
 	l.logger.Info(args...)
 }
 
-func (l *ConsoleLogger) Warn(args ...interface{}) {
+func (l *consoleLogger) Warn(args ...interface{}) {
 	l.logger.Warn(args...)
 }
 
-func (l *ConsoleLogger) Error(args ...interface{}) {
+func (l *consoleLogger) Error(args ...interface{}) {
 	l.logger.Error(args...)
 }
 
-func (l *ConsoleLogger) Fatal(args ...interface{}) {
+func (l *consoleLogger) Fatal(args ...interface{}) {
 	l.logger.Fatal(args...)
 }
 
-func (l *ConsoleLogger) Panic(args ...interface{}) {
+func (l *consoleLogger) Panic(args ...interface{}) {
 	l.logger.Panic(args...)
 }
 
-// FileLogger is an implementation of Logger that logs to a file.
-type FileLogger struct {
+// fileLogger is an implementation of Logger that logs to a file.
+type fileLogger struct {
 	logger *logrus.Logger
 }
 
 // NewFileLogger creates a new file logger with the specified log level and file path.
-func NewFileLogger(level logrus.Level, filePath string) *FileLogger {
+func NewFileLogger(level logrus.Level, filePath string) *fileLogger {
 	logger := logrus.New()
 
 	logger.SetOutput(&lumberjack.Logger{
@@ -73,26 +73,26 @@ func NewFileLogger(level logrus.Level, filePath string) *FileLogger {
 		FullTimestamp: true,
 	})
 	logger.SetLevel(level)
-	return &FileLogger{logger: logger}
+	return &fileLogger{logger: logger}
 }
 
-func (l *FileLogger) Info(args ...interface{}) {
+func (l *fileLogger) Info(args ...interface{}) {
 	l.logger.Info(args...)
 }
 
-func (l *FileLogger) Warn(args ...interface{}) {
+func (l *fileLogger) Warn(args ...interface{}) {
 	l.logger.Warn(args...)
 }
 
-func (l *FileLogger) Error(args ...interface{}) {
+func (l *fileLogger) Error(args ...interface{}) {
 	l.logger.Error(args...)
 }
 
-func (l *FileLogger) Fatal(args ...interface{}) {
+func (l *fileLogger) Fatal(args ...interface{}) {
 	l.logger.Fatal(args...)
 }
 
-func (l *FileLogger) Panic(args ...interface{}) {
+func (l *fileLogger) Panic(args ...interface{}) {
 	l.logger.Panic(args...)
 }
 
